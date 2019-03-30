d4f61dfa8c9e49bddf302771ba9166cdb723a6bd
QuangHuy <quanghuy27101997@gmail.com>
2019-03-31 00:14:46 +0700

    Create Map

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Map : MonoBehaviour{
+	public List<Segment> listSegment = new List<Segment>();
+
+	public void initSegements(){
+	
+		this.listSegment.Add (new Segment (new Vector2 (-5, 0), new Vector2 (5, 0)));
+
+	}
+
+	void Start() {
+
+		initSegements ();
+		JsonUtility.ToJson (this.listSegment);
+
+	}
+}
---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment {
+
+	public Vector2 startPoint;
+	public Vector2 endPoint;
+
+	public Segment(Vector2 s,Vector2 e){
+
+		if (s.x > e.x || s.y > e.y) {
+			Vector2 t = s;
+			s = e;
+			e = t;
+		};
+
+		this.startPoint = s;
+		this.endPoint = e;
+	}
+}
