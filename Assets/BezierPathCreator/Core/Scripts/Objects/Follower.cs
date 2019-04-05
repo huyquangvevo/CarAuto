cc904d5548413848126fbffa408aff5993ddfa5a
QuangHuy <quanghuy27101997@gmail.com>
2019-04-05 09:52:45 +0700

    Create Collider

---
+++ Assets/BezierPathCreator/Core/Scripts/Objects/Follower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour {
+
+	public PathCreator pathCreator;
+
+	void Start () {
+		Debug.Log (pathCreator.path.GetPointAtDistance(50));
+	}
+}
--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -18,8 +18,24 @@ public class Drive : MonoBehaviour {
 	}
 
 	void driveCar(){
+	//	Vector2 sp = new Vector2 (-5, 0);
+	//	Vector2 ep = new Vector2 (5, 1);
+		this.rb.velocity = new Vector2 (0.2f,0);
+	//	this.transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 135));
+	}
 
-		this.rb.velocity = new Vector2 (1.0f, 0f);
-		
+	void OnCollisionStay2D(Collision2D col){
+		Debug.Log ("Contact: " + col.contactCount);
+		Debug.Log ("Va Cham " + col.contacts.Length);
+		for (int i = 0; i < col.contacts.Length; i++) {
+			Debug.Log ("Diem va cham " + i + " " + col.GetContact(i).point);
+		}
+		//Debug.Log ("Diem va cham: " + col.GetContact(0).point);
+		//if(col.contacts.Length>1)
+		//	Debug.Log ("Diem va cham 2: " + col.GetContact(1).point);
+	}
+
+	void OnTriggerEnter2D(Collider2D coli){
+		Debug.Log ("Cham Trigger");
 	}
 }
